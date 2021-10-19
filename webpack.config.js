8b37ea291e60a53e68d98c1af8e30719df808c3a
laianribas <laian67a@gmail.com>
2021-10-19 09:43:51 -0300

    Erro no npm start

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080
+  },
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(__dirname, './src/index.js')
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'webpack Boilerplate',
       template: path.resolve(__dirname, './src/html/template.html'), // template file
-      filename: 'index.html', // output file
+      filename: 'index.html' // output file
     }),
 
     new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
 
   module: {
@@ -27,22 +37,22 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ['babel-loader']
       },
       //imagens
-       {
+      {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       //fonts and svg
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      }, 
+        type: 'asset/inline'
+      },
       {
         test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      }, 
-    ],
-  },
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ]
+  }
 }
